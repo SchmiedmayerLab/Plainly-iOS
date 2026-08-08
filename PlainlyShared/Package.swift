17c51e4c1940bcb4b2529a4a6a6486e1d265087c
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-08-08 01:22:49 -0700

    Align Spezi Package Revisions

--- PlainlyShared/Package.swift
+++ PlainlyShared/Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/SchmiedmayerLab/Spezi.git",
-            revision: "cbd54fe572652dbf6e1475d87460e79e56cffbd6"
+            revision: "b5ce1a15688afffd83d7f757884e86e17a65b65c"
         ),
         .package(url: "https://github.com/apple/FHIRModels.git", .upToNextMinor(from: "0.9.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0")
